3961255f525b98bc50c441d9213cb4e6e6543f73
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2016-10-17 21:40:16 +0200

    avoid error with DNN cachebuster ver=xxx

---
+++ ValidatingRequest.cs
@@ -0,0 +1,29 @@
+﻿using DotNetNuke.Web.Api;
+using ImageProcessor.Web.HttpModules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Satrabel.OpenImageProcessor
+{
+    public class ValidatingRequest : IServiceRouteMapper
+    {
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+        {
+            ImageProcessingModule.ValidatingRequest += (sender, args) =>
+            {
+                if (!string.IsNullOrWhiteSpace(args.QueryString))
+                {
+                    var queryCollection = HttpUtility.ParseQueryString(args.QueryString);
+                    // ignore cachebuster
+                    if (queryCollection.AllKeys.Contains("ver"))
+                    {
+                        queryCollection.Remove("ver");
+                        args.QueryString = queryCollection.ToString();
+                    }
+                }
+            }; 
+        }
+    }
+}

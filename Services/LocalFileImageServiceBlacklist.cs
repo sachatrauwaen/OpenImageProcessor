c84d95fed53ed0c1cd3813eb2bb10ed3ba37adbb
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2018-11-09 10:44:56 +0100

    Add missing Microsoft.IO.RecyclableMemoryStream.dll in dnn manifest Add LocalFileImageServiceBlacklist

---
+++ Services/LocalFileImageServiceBlacklist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Web;
+using System.Text.RegularExpressions;
+using ImageProcessor.Web.Services;
+
+namespace Satrabel.OpenImageProcessor.Services
+{
+    public class LocalFileImageServiceBlacklist : LocalFileImageService
+    {
+        public override bool IsValidRequest(string path)
+        {
+
+            if (this.Settings["BlacklistRegex"] != null)
+            {
+                Regex rgx = new Regex(this.Settings["BlacklistRegex"]);
+
+                if (rgx.IsMatch(path))
+                {
+                    return false;
+                }
+            }
+
+            return base.IsValidRequest(path);
+        }
+    }
+}
